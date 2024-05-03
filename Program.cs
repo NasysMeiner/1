769d49efa7b21317978d1f29f18d3a523becd91b
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2024-05-03 15:54:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            string baseStr = "компьютер";
+
+            while (isWork)
+            {
+                Console.WriteLine("Enter number");
+                string numStr = Console.ReadLine();
+                string finalStr = "" + baseStr;
+                int number = Convert.ToInt32(numStr) % 10;
+
+                if (number == 2 || number == 3)//Конструкция, которая проверяет на склонение
+                    finalStr += "а";
+                else if (number != 1)
+                    finalStr += "ов";
+
+                numStr += $" {finalStr}";
+
+                Console.Clear();
+                Console.WriteLine($"Final result: {numStr}\nEnter '-' to exit");//Вывод результата
+                numStr = Console.ReadLine();
+
+                if (numStr == "-")//Выход из бесконечного цикла
+                    break;
+            }
+        }
+    }
+}
